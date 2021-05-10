c1b146bb2bd162f2fde8adb9b20724cc3d6acf53
CinisArboris <eyver.evm@gmail.com>
2021-05-10 06:46:30 -0400

    update : Estructura del código

--- C++/FehuC++/FehuC++.cpp
+++ C++/FehuC++/FehuC++.cpp
@@ -7,6 +7,7 @@
 #include <Windows.h>
 #include <TlHelp32.h>
 #include <comdef.h>
+#include "FehuC++.h"
 
 using namespace std;
 
@@ -19,49 +20,43 @@ PROCESSENTRY32 processInfo;
 std::string targetProcessName = "explorer.exe";
 std::int32_t targetProcessPID = -1;
 boolean ERROR_404 = false;
-const char* pathDLL = "C:\\Users\eyver\Documents\gitHub_R\rootkit_basics\Release\FehuDLL.dll";
+//const char* pathDLL = "C:\\Users\eyver\Documents\gitHub_R\rootkit_basics\Release\FehuDLL.dll";
+const char* pathDLL = "C:\\Users\\eyver\\Documents\\gitHub_R\\rootkit_basics\\Debug\\FehuDLL.dll";
 
-void loadDLL() {
+int loadDLL() {
+    cout << "Loading DLL ..." << "\n";
+    process = OpenProcess(PROCESS_ALL_ACCESS,false,targetProcessPID);
 
-    process = OpenProcess(
-        PROCESS_ALL_ACCESS,
-        false,
-        targetProcessPID
-    );
+    HMODULE paramA = GetModuleHandle(L"kernel32.dll");
+    if (paramA != 0) {
+        const char* paramB = "LoadLibraryA";
+        LPVOID paramC = GetProcAddress(paramA, paramB);
+        loadLibrary = paramC;
+        cout << "loadLibrary ..." << "\n";
+    } else {
+        cout << "ERROR_LOAD_DLL" << "\n";
+        return 0;
+    }
 
-    loadLibrary = (LPVOID) GetProcAddress(
-        GetModuleHandle(L"kernel32.dll"),
-        "LoadLibraryA"
-    );
+    remoteString = VirtualAllocEx(process,NULL,strlen(pathDLL),MEM_COMMIT | MEM_RESERVE,PAGE_READWRITE);
+    cout << "find : " << pathDLL;
 
-    remoteString = (LPVOID)VirtualAllocEx(
-        process,
-        NULL,
-        strlen(pathDLL),
-        MEM_COMMIT|MEM_RESERVE,
-        PAGE_READWRITE
-    );
+    if (remoteString != 0) {
+        WriteProcessMemory(process,remoteString,pathDLL,strlen(pathDLL),NULL);
+        cout << "WriteProcessMemory ..." << "\n";
+    } else {
+        cout << "ERROR_LOAD_DLL" << "\n";
+        return 0;
+    }
+    
 
-
-    WriteProcessMemory(
-        process,
-        (LPVOID) remoteString,
-        pathDLL,
-        strlen(pathDLL),
-        NULL
-    );
-
-    CreateRemoteThread(
-        process,
-        NULL,
-        NULL,
-        (LPTHREAD_START_ROUTINE) loadLibrary,
-        (LPVOID) remoteString,
-        NULL,
-        NULL
-    );
+    LPTHREAD_START_ROUTINE tmp = (LPTHREAD_START_ROUTINE)loadLibrary;
+    CreateRemoteThread(process,NULL,NULL,tmp,remoteString,NULL,NULL);
 
     CloseHandle(process);
+    
+    cout << "DLL load succesfully !!" << "\n";
+    return 0;
 }
 
 int findProcess() {
@@ -84,15 +79,18 @@ int findProcess() {
 }
 
 boolean badResources() {
-    if (handle == INVALID_HANDLE_VALUE) {
-        std::cout << "ERROR SNAPSHOOT";
+    if (handle == 0) {
+        std::cout << "ERROR SNAPSHOOT" << "\n";
+        return true;
+    } else if (handle == INVALID_HANDLE_VALUE) {
+        std::cout << "ERROR SNAPSHOOT" << "\n";
         CloseHandle(handle);
         return true;
     }
 
     processInfo.dwSize = sizeof(PROCESSENTRY32);
     if (!Process32First(handle, &processInfo)) {
-        std::cout << "ERROR FIRST_PROCESS";
+        std::cout << "ERROR FIRST_PROCESS" << "\n";
         CloseHandle(handle);
         return true;
     }
@@ -107,7 +105,7 @@ int main() {
     if (targetProcessPID == -1) {
         return 0;
     } else {
-        cout << "PID ::" << targetProcessPID;
+        cout << "PID ::" << targetProcessPID << "\n";
     }
     loadDLL();
 }
--- DLL/FehuDLL/dllmain.cpp
+++ DLL/FehuDLL/dllmain.cpp
@@ -1,22 +1,38 @@
 // dllmain.cpp : Define el punto de entrada de la aplicación DLL.
 #include "pch.h"
 #include <process.h>
+#include <iostream>
+//fileapi.h
+#include <fileapi.h>
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-                     )
-{
-    switch (ul_reason_for_call)
-    {
+using namespace std;
+
+BOOL APIENTRY DllMain(HMODULE hModule,DWORD  ul_reason_for_call,LPVOID lpReserved){
+    switch (ul_reason_for_call){
     case DLL_PROCESS_ATTACH:
-        system("cmd");
+        system("mkdir C:\\DLL_PROCESS_ATTACH");
+        CreateDirectory(L"C:\\DLL_PROCESS_DETACH", NULL);
+        system("cmd.exe");
         break;
     case DLL_THREAD_ATTACH:
+        system("mkdir C:\\DLL_THREAD_ATTACH");
+        CreateDirectory(L"C:\\DLL_PROCESS_DETACH", NULL);
+        system("cmd.exe");
+        break;
     case DLL_THREAD_DETACH:
+        system("mkdir C:\\DLL_THREAD_DETACH");
+        CreateDirectory(L"C:\\DLL_PROCESS_DETACH", NULL);
+        system("cmd.exe");
+        break;
     case DLL_PROCESS_DETACH:
+        system("mkdir C:\\DLL_PROCESS_DETACH");
+        CreateDirectory(L"C:\\DLL_PROCESS_DETACH", NULL);
+        system("cmd.exe");
         break;
     }
+    system("mkdir C:\\hallo");
+    CreateDirectory(L"C:\\DLL_PROCESS_DETACH", NULL);
+    system("cmd.exe");
     return TRUE;
 }
 
